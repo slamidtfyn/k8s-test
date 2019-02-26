67cfa3c8f291396981cbac4687ab842595d25c19
Søren Larsen <sl@komit.nu>
2019-02-27 00:08:44 +0100

    base

---
+++ frontend/Pages/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using StackExchange.Redis;
+
+namespace frontend.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string Message {get;private set;}
+        public void OnGet()
+        {
+         Random rnd=new Random();
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost:32768");
+            var db = redis.GetDatabase();
+            int x = rnd.Next(1,600);
+            string value = Guid.NewGuid().ToString();
+            string key = $"work{x}";
+            Message=$"{key}:{value}";
+            db.SetAdd(key, value);
+        }
+    }
+}
---
+++ worker/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using StackExchange.Redis;
+
+namespace worker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost:32768");
+            var workers = Enumerable.Range(1, 600).ToList();
+            HttpClient c = new HttpClient();
+            c.BaseAddress = new Uri("https://localhost:5003");
+            var db = redis.GetDatabase();
+            while (true)
+            {
+                Thread.Sleep(5);
+                foreach (var d in workers.AsParallel())
+                {
+                    var item = db.SetPop($"work{d}");
+                    if (!item.IsNull)
+                    {
+                        try
+                        {
+                            var task = new Task(() =>
+                            {
+                                c.PostAsync("api/values?value=" +item.ToString(),new StringContent(""));
+                                Console.WriteLine($"work{d}:{item.ToString()}");
+                            }
+                            );
+                            task.Start();
+                            task.Wait();
+                            if (task.IsFaulted) throw task.Exception;
+                        }
+                        catch (System.Exception)
+                        {
+                            db.SetAdd($"work{d}", item);
+                            throw;
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+}
